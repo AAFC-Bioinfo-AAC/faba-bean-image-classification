2f20fd5522163f16ef4ff17aa708fe4632a05342
Harpreet Bargota <harpreet.bargota@agr.gc.ca>
2024-07-17 17:18:59 +0000

    Replace feature_extraction.py

--- harpreet_scripts/feature_extraction.py
+++ harpreet_scripts/feature_extraction.py
@@ -1,3 +1,4 @@
+#hkb
 #__author__="harpreet kaur bargota"
 #__email__="harpreet.bargota@agr.gc.ca"
 #__Project__="WGRF - Image Classification pipeline for Faba beans"
@@ -17,65 +18,41 @@
 ###Image classification pipeline using amg SAM, feature extraction and classification (to be done)
 
     
-    ## for loop for pipeline
-import torch
+    ## Import the required libraries
+
 import argparse
-import matplotlib.pyplot as plt
+import os as os
 import cv2
+import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 import seaborn as sns
 import skimage
-from skimage import filters, io, color
-from skimage.io import imread
-from skimage import filters
-from skimage import measure
-#from pyclesperanto_prototype import imshow
-from skimage.color import rgb2gray
-from glob import glob
-import os as os
-import warnings
-from skimage.measure import label, regionprops, regionprops_table
-# Ignore all warnings
-warnings.filterwarnings("ignore")
-import numpy as np
-import torch
-import matplotlib.pyplot as plt
-import cv2
 import supervision as sv
+
 from skimage.util import crop
-from pycocotools import mask as mask_utils
+from skimage import measure
+from skimage.measure import label, regionprops, regionprops_table
+
 from typing import Any, Dict, List, Optional, Tuple
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
+
+
+# Ignore all warnings
+import warnings
+warnings.filterwarnings("ignore")
+
 import sys
 sys.path.append("..")
 
 
 
-
 #Dependencies for SAM
-from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
+
 sam_checkpoint = "sam_vit_h_4b8939.pth"
 model_type = "vit_h"
 device = "cuda"    
 
-
-
-
-
-## defining parameters for masks
-def show_anns(anns):
-    if len(anns) == 0:
-        return
-    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
-    ax = plt.gca()
-    ax.set_autoscale_on(False)
-
-    img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
-    img[:,:,3] = 0
-    for ann in sorted_anns:
-        m = ann['segmentation']
-        color_mask = np.concatenate([np.random.random(3), [0.35]])
-        img[m] = color_mask
-    ax.imshow(img)
 
 # defining the function for processing of images
 
@@ -91,6 +68,9 @@ def process_images(input_folder, output_folder):
 		os.makedirs(output_folder)
 		print(f"Output folder '{output_folder}' created.")
 
+	
+	
+	#Iteration loop for processing images
 	for root, dirs, files in os.walk(input_folder):
 		df_total = []
 		
@@ -98,11 +78,10 @@ def process_images(input_folder, output_folder):
 
 #Step1:        # Read an image
 			image_path=os.path.join(root,file)
-			image_path = os.path.join(root,file)
 			image=cv2.imread(image_path)
 			image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)   
-			#Crop the image part having colorcard 
-			image = crop(image, ((2200, 800), (50, 50), (0,0)), copy=False)
+			image = crop(image, ((2200, 500), (0, 0), (0,0)), copy=False)
+			
 
 #Step2: 	#Apply SegmentAnything model 
  
@@ -110,8 +89,8 @@ def process_images(input_folder, output_folder):
 			sam.to(device=device) 
 			mask_generator = SamAutomaticMaskGenerator(sam, box_nms_thresh = 0.1, output_mode = "binary_mask") 
 			masks_SAM = mask_generator.generate(image)  
-			print("The total length of all masks is: ", len(masks_SAM))
-			print(masks_SAM[0].keys())
+			print ("The total length of all masks is: ", len(masks_SAM))
+			
 			
             # Metadata
 			
@@ -137,33 +116,39 @@ def process_images(input_folder, output_folder):
  #Step3:           
     # Filtering the masks of beans:
 
-			#eliminating the masks for label
-			df_metadata = df_metadata.drop(df_metadata[(df_metadata['bbox_x0'] <= 1600) & (df_metadata['bbox_y0'] >= 2400)].index) 
+			# dataframe for coin required for standardization of area, length and width
+			df_metadata_coin = df_metadata[(df_metadata['bbox_x0'] >= 3300) & (df_metadata['area'] >= 200000)]
+			# Find the mask of coin
+			Mask_index= df_metadata_coin.index.tolist()
+			masks_coin=[masks_SAM[x] for x in Mask_index]
+			print ('index of coin mask is ', Mask_index)
 			
-			#eliminating the masks for coin
-			df_metadata = df_metadata.drop(df_metadata[(df_metadata['bbox_x0'] >= 3300) & (df_metadata['bbox_y0'] >= 2000)].index)
+			#Eliminating the masks of colorcard, label, scale, coin, and duplicate masks for seeds
+			conditions = [(df_metadata['bbox_x0'] <= 1900) & (df_metadata['bbox_y0'] >= 2500), #label
+    		(df_metadata['bbox_x0'] <= 4000) & (df_metadata['bbox_y0'] >= 3000), #Scale
+    		(df_metadata['bbox_x0'] >= 3300) & (df_metadata['bbox_y0'] >= 2400), #coin
+   			(df_metadata['bbox_h'] >= 650), #duplicate mask for seed
+    		(df_metadata['bbox_w'] >= 650) #duplicate mask for seed
+			]
 			
-			#eliminating the duplicate masks for beans in the same box -- 'bbox_h' and 'bbox_w' for height and width of the box
-			df_metadata = df_metadata.drop(df_metadata[(df_metadata['bbox_h'] >= 600)].index) 
-			df_metadata = df_metadata.drop(df_metadata[(df_metadata['bbox_w'] >= 600)].index) 
-			
-			#Considering the minimum and maximum area of beans
-			df_metadata=df_metadata.loc[(df_metadata['area'] >= 5000) & (df_metadata['area'] <= 300000)]
+			for condition in conditions:
+				df_metadata = df_metadata.drop(df_metadata[condition].index)
 			print (df_metadata)
-			             
-	    # Saving the metadata for the masks of beans to the output folder
+
+			
+
+
+
+	    	# Saving the metadata for the masks of beans to the output folder
 			image_filename = os.path.basename(image_path)
 			output_filename = f"Metadata_{image_filename}.xlsx"
 			output_path= os.path.join(output_folder, output_filename)
 			df_metadata.to_excel(output_path)
-			Total_number_of_seeds=len(df_metadata.index)
-			print("Total number of seeds is ", Total_number_of_seeds)
+
+						
+		# Save fava bean masks in output_folder
 			Masks_list=df_metadata.index.tolist()
 			masks_SAM=[masks_SAM[x] for x in Masks_list]
-		
-            
-				
-			# Save fava bean masks in output_folder
 			for i, mask_data in enumerate(masks_SAM):
 				mask = mask_data["segmentation"]
 				image_filename = os.path.basename(image_path)
@@ -183,12 +168,38 @@ def process_images(input_folder, output_folder):
 			output_filename = f"Annotated_image_{image_filename}.png"
 			output_path= os.path.join(output_folder, output_filename)
 			cv2.imwrite(output_path, annotated_frame)
+
+
+			#Standardize 
+			df_coin=[]
+			for i, mask_data in enumerate(masks_coin):
+				mask=mask_data["segmentation"]
+				label_image = measure.label(mask)
+				props = regionprops_table(label_image,properties=('area','perimeter','axis_major_length', 'axis_minor_length')) 
+				coin_std = pd.DataFrame(props)
+
+				Length_coin_mm=23.88
+				width_coin_mm=23.88
+
+				Area_standard_coin_pixels= coin_std.iloc[0]['area'] # Area of coin in pixels
+				Area_Standard_coin_mm2=3.14*(Length_coin_mm/2)*(Length_coin_mm/2) # Area of coin in mm2
+				Calibration_factor_area=(Area_Standard_coin_mm2/Area_standard_coin_pixels) # Calibration factor for area
+
+				axis_major_length_pixels= coin_std.iloc[0]['axis_major_length'] # Length of coin in pixels
+				Calibration_factor_length=(Length_coin_mm/axis_major_length_pixels) # Calibration factor for length
+
+				axis_minor_length_pixels= coin_std.iloc[0]['axis_minor_length'] # Width of coin in pixels
+				Calibration_factor_width=(width_coin_mm/axis_minor_length_pixels) # Calibration factor for width
+
+				perimeter_mm =(2*3.14*Length_coin_mm)/2
+				perimeter_pixels= coin_std.iloc[0]['perimeter'] # perimeter of coin in pixels
+				Calibration_factor_perimeter=(perimeter_mm/perimeter_pixels) # Calibration factor for perimeter
 #Step5:			
 # FEATURE EXTRACTION
 			df_list=[]
 			for i, mask_data in enumerate(masks_SAM):
 				mask = mask_data["segmentation"]
-
+			
 				#Label the mask
 				#Reference: https://scikit-image.org/docs/stable/auto_examples/segmentation/plot_label.html
 				label_image = measure.label(mask)
@@ -201,6 +212,11 @@ def process_images(input_folder, output_folder):
 				df_FE = pd.DataFrame(props)
 				
 				#For calculating the other parameters, mathematical operations have been used as advised in paper(Multiclass classification of dry beans using computer vision and machine learning techniques)
+				df_FE["Area_mm2"]=df_FE["area"] * Calibration_factor_area
+				df_FE["Length_mm"]=df_FE["axis_major_length"] * Calibration_factor_length
+				df_FE["Width_mm"]=df_FE["axis_minor_length"] * Calibration_factor_width
+				df_FE["perimeter_mm2"]=df_FE["perimeter"] * Calibration_factor_perimeter
+				
 				df_FE["Aspect_Ratio"] = df_FE["axis_major_length"]/df_FE['axis_minor_length']
 				df_FE["Roundness"]= (4*3.14*(df_FE["area"]))/((df_FE["perimeter"])**2)
 				df_FE["Compactness"] = df_FE["equivalent_diameter_area"]/df_FE["axis_major_length"]
@@ -215,10 +231,13 @@ def process_images(input_folder, output_folder):
 				class_in_image=(image_path).split('.JPG')[0]
 				df_FE["class"]= (image_path).split('.JPG')[0]
 				df_list.append(df_FE)
+			
 			df_FE2 = pd.concat(df_list)
 			df_total.append(df_FE2)
 		df_image=pd.concat(df_total)
 		print(df_image)
+ 
+
 
 #Save the final file of feature extraction from all images into output folder
 		output_filename = f"Fava_bean_Features_extraction.xlsx"
@@ -249,5 +268,8 @@ if __name__ == "__main__":
 
 #for x in args.input_folder:
 #	image = cv2.imread(r'x')   # read an image using openCV
+
+
+
 
 
